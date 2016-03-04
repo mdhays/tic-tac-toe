96b6d81a3bd20ed17213c1978681309233777d3a
Matt Hays <mdhays1995@gmail.com>
2016-03-04 15:25:08 -0600

    User moves pushing to an array that keeps track of moves.

--- public/js/logic.js
+++ public/js/logic.js
@@ -2,14 +2,41 @@
 
 let player = 4
 
+const winningRows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+]
+
+const winningColumns = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]   
+
+const winningDiagonals = [
+  [0, 4, 8],
+  [2, 4, 6]
+]
+
+let playerX = []
+let playerO = []
+
 function turn(btn) {
   
   if(player % 2 === 0) {
-    console.log('x')
-  document.getElementById(btn).value = 'X'
-  player += 1
+    // console.log('x')
+
+    
+    playerX.push(btn)
+    console.log(playerX)
+    document.getElementById(btn).value = 'X'
+    player += 1
   } else {
     console.log('o')
+    playerO.push(btn)
+    console.log(playerO)
+
     document.getElementById(btn).value = 'O'
     player += 1
   }
