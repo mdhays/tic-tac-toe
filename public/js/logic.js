95572da44b80c7333519a91052749ae0a60a21ef
Matt Hays <mdhays1995@gmail.com>
2016-03-05 14:02:54 -0600

    Removed string comparison code, and added a new function that tracks remaining turns.

--- public/js/logic.js
+++ public/js/logic.js
@@ -1,8 +1,24 @@
 'use strict'
 
 let player = 4
+let turnsRemaining = 8
 
-const winningArrs = ['012', '345', '678', '036', '147', '258', '048', '246']
+const winningRows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+]
+
+const winningColumns = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8]
+]   
+
+const winningDiagonals = [
+  [0, 4, 8],
+  [2, 4, 6]
+]
 
 let playerX = []
 let playerO = []
@@ -17,24 +33,35 @@ function turn(btn) {
     console.log(playerX)
     document.getElementById(btn).value = 'X'
     player += 1
-
-    checkWinner(playerX)
+    countDownTurns()
   } else {
-    console.log('o')
+    
     playerO.push(btn)
     console.log(playerO)
 
     document.getElementById(btn).value = 'O'
     player += 1
-
-    checkWinner(playerO)
+    countDownTurns()
   }
 }
 
-function checkWinner(dudeArray) {
-  let newarr = dudeArray.join('')
-  if(dudeArray.length === 3 && winningArrs.indexOf(newarr) !== -1) {
-    console.log('what a play')
+function countDownTurns() {
+  turnsRemaining -= 1
+  console.log(turnsRemaining)
+  if (turnsRemaining === 0) {
+    alert('game over')
   }
+}
+
+function checkForWinner(playerArr) {
 
 }
+
+
+
+
+
+
+
+
+
